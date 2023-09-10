e3ebf1437d5a76ecca54cfb3485b44b47c2d2051
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-09-11 11:38:19 +1200

    Monday Tutorial Progress

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -6,8 +6,9 @@ const SLICE_COUNT = 8;
 
 //how phenakistoscope runs as a whole
 function setup_pScope(pScope){
-  pScope.output_mode(STATIC_FRAME); //STATIC_FRAME / ANIMATED_FRAME / STATIC_DISK / ANIMATED_DISK / OUTPUT_GIF(1000) / OUTPUT_PRINT(A4orA3)
-  pScope.scale_for_screen(true);
+  pScope.output_mode(ANIMATED_DISK); //STATIC_FRAME / ANIMATED_FRAME / STATIC_DISK / ANIMATED_DISK / OUTPUT_GIF(1000) / OUTPUT_PRINT(A4orA3)
+// pScope.output_mode(OUTPUT_PRINT(A3));
+ pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false); 
   pScope.set_direction(CCW); //CW or CCW - inward or outward
   pScope.set_slice_count(SLICE_COUNT);
@@ -126,34 +127,34 @@ translate(xSwoop, ySwoop);
 scale(scaleVal2);
 
 push()
-scale(scaleVal2*1.25)
-pScope.draw_image("eagleTail", 0, 1000);
+scale(scaleVal2*12.5)
+pScope.draw_image("eagleTail", -10, 100);
 pop()
 
 push()
-scale(1.7);
-pScope.draw_image("clawL", -400, 950);
-pScope.draw_image("clawR", 400, 950);
+scale(4);
+pScope.draw_image("clawL", -220, 420);
+pScope.draw_image("clawR", 170, 420);
 pop()
 
 push()
 push();
-translate(-1000, 100);
-scale(scaleVal2*1.75)
+translate(-400, -50);
+scale(scaleVal2*20)
 rotate(rotateWing);
-pScope.draw_image("wingL", -1500, 0);
+pScope.draw_image("wingL", -250, 0);
 pop();
 push();
-translate(1000, 100);
-scale(scaleVal2*1.75)
+translate(400, -50);
+scale(scaleVal2*20)
 rotate(-rotateWing);
-pScope.draw_image("wingR", 1500, 0);
+pScope.draw_image("wingR", 60, 0);
 pop();
 pop()
 
 push()
-scale(scaleVal2*1.4)
-pScope.draw_image("eagleHead", -50, 0);//ySwoop + -10000); 
+scale(10)
+pScope.draw_image("eagleHead", -15, 0);//ySwoop + -10000); 
 pop()
 pop()
 pop()
@@ -183,6 +184,18 @@ rotate(350)
   endShape();
 
   rect(-40, -1, 40, 90)
+
+  fill(114, 191, 86);
+  noStroke();
+  beginShape();
+  curveVertex(0, 0);
+  curveVertex(0, 0);
+  curveVertex(-10, -12);
+  curveVertex(-20, -15);
+  curveVertex(-30, -12)
+  curveVertex(-40, 0);
+  curveVertex(-40, 0);
+  endShape();
 pop()
 }
 
@@ -196,7 +209,7 @@ function cactus(x, y, animation, pScope){
 
   let scaleVal = 0.08;
 
-rotate(350);
+// rotate(350);
 
 push()
 scale(scaleVal);
@@ -233,8 +246,10 @@ function tumbleweedCentre(x, y, animation, pScope){
 
 function horseJump(x, y, animation, pScope){
   push()
-    let scaleVal = 0.1;
+    let scaleVal = 0.2;
     let jumpFrames = 0.45;
+    let horseStart = -2330;
+    let horseBounce = -4330;
     rotate(8);
   
   
@@ -246,14 +261,14 @@ function horseJump(x, y, animation, pScope){
     if(animation.frame<=jumpFrames){//horse jump in first section of animation (jumpframes is at top of code, currently 0.45)
       let horseY;//create y val for horse
       if(animation.frame<=jumpFrames/2){//jump up
-        horseY = map(animation.frame, 0, jumpFrames/2, -4660, -5000);
+        horseY = map(animation.frame, 0, jumpFrames/2, horseStart, horseBounce);
       }
       else{ //jump fall
-        horseY = map(animation.frame, jumpFrames/2, jumpFrames, -5000, -4660);
+        horseY = map(animation.frame, jumpFrames/2, jumpFrames, horseStart, horseBounce-1000);
       }
 
       // horseY = -4660;
-      pScope.draw_image("horseGrey", 1900 - animation.frame*1600, horseY); //400, -4660 is the horse end pos. you may want to tweak start pos (currently 300, -5000)
+      pScope.draw_image("horseGrey", 1320-animation.frame*1600, horseY); //400, -4660 is the horse end pos. you may want to tweak start pos (currently 300, -5000)
     }
     // pScope.draw_image("horse", 400, -4660); //400, -4660 end pos
     pop()
@@ -262,20 +277,26 @@ function horseJump(x, y, animation, pScope){
   }
 
 
-
+  // let horseStart = -2330;
+  // let horseEndX = -6000;
+  // let horseEndY = -400;
 
 
 function horse(x, y, animation, pScope){
 
-  let scaleVal = 0.1;
+  let scaleVal = 0.2;
   let jumpFrames = 0.45;
   let xOffset = 1000;
+  let horseStartX = -400;
+  let horseStartY = -2415;
+  let horseEndX = -6000;
+  let horseEndY = -400;
 
   scale(scaleVal);
   if(animation.frame>=jumpFrames){ 
 //-4660, 400 horse start pos, -3000, -8660 end pos - probably tweak end points based on eagle, easiest way to do so is in static_disc mode
-  let yLift = map(animation.frame, jumpFrames, 1, -4600+500, -8660); //500 is an offset to tweak start pos based on frame no
-  let xLift = map(animation.frame, jumpFrames, 1, 400+xOffset, -3000); // 1000 is an offset to tweak start pos based on frame no
+  let yLift = map(animation.frame, jumpFrames, 1, horseStartY+500, horseEndX); //500 is an offset to tweak start pos based on frame no
+  let xLift = map(animation.frame, jumpFrames, 1, horseStartX+xOffset, horseEndY); // 1000 is an offset to tweak start pos based on frame no
 
   translate(xLift, yLift); //used a translate for the horse position in case you want to animate the rotation/scale as well
     rotate(8);
